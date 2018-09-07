e971bcba5fb5b4558d211d214989966aa8211486
admin <admin@admin.com>
2018-09-07 13:03:25 +0530

    new changes

--- src/actions/konakart/DBVerification.java
+++ src/actions/konakart/DBVerification.java
@@ -3,24 +3,36 @@ package actions.konakart;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import static org.junit.Assert.*;
 
 import java.util.*;
 
 class DBVerification{
-    public void run(HashMap<String, Object> params)  throws ClassNotFoundException, SQLException{
-        System.out.println("We are now inside ");
-       for(String keys:params.keySet())
-	      {
-	    	  System.out.println(keys+":  "+params.get(keys).toString());
-	      }
+    public void run(HashMap<String, String> params)  throws ClassNotFoundException, SQLException{
         Connection conn;
-        String dbURL = "jdbc:mysql://"+ params.get("host").toString() +":"+  params.get("port").toString();
-	        Class.forName("com.mysql.jdbc.Driver");
-	        conn = DriverManager.getConnection(dbURL,  params.get("user").toString(),  params.get("pass").toString());
-	   Statement stmt = conn.createStatement();
-	     ResultSet rs = stmt.executeQuery(params.get("sql").toString());
-	      conn.close();
+        Class.forName("com.mysql.cj.jdbc.Driver");
+		String connectionURL = "jdbc:mysql://" + params.get("host") + ":" + params.get("port") + "/" + params.get("dbName") + "?autoReconnect=true&useSSL=false";
+	    conn = DriverManager.getConnection(connectionURL,  params.get("user"),  params.get("pass"));
+	    Statement stmt = conn.createStatement();
+	    ResultSet rs = stmt.executeQuery(params.get("sql"));
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnsNumber = rsmd.getColumnCount();
+        String actualValue = "";
+		while (rs.next()) {
+			for (int i = 1; i <= columnsNumber; i++) {
+                if(i==2)
+                {
+                   actualValue = rs.getString(i);
+                   break;
+                }
+			}
+        }
+        assertEquals(params.get("uiValue"),actualValue);
+		rs.close();
+		conn.close();
     }
 }
