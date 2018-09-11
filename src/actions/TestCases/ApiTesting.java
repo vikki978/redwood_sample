00bd6c2b2cecdb3baed0027906239ef9f238469f
admin <admin@admin.com>
2018-09-11 16:07:57 +0530

    api test changes

--- src/actions/TestCases/ApiTesting.java
+++ src/actions/TestCases/ApiTesting.java
@@ -1,11 +1,8 @@
 package actions.TestCases;
 
 import io.restassured.RestAssured;
-import io.restassured.http.Method;
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-
-import static org.junit.Assert.*;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 import java.util.*;
 
@@ -13,11 +10,9 @@ class ApiTesting{
     public void run(HashMap<String, Object> params){
         
         RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
-		RequestSpecification httpRequest = RestAssured.given();
-		Response response = httpRequest.request(Method.GET, "/Hyderabad");
-		String responseBody = response.getBody().asString();
-		System.out.println("Response Body is =>  " + responseBody);
-        assertTrue(responseBody.contains("Hyderabad"));
+
+		 given().when().get("/Hyderabad")
+		  .then().assertThat().body("City", equalTo("Hyderabad"));
         
     }
 }
