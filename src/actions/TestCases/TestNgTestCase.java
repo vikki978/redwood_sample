2b413b7b9ee88d7717a8597f36f80207fbefa5a0
admin <admin@admin.com>
2018-09-12 10:16:12 +0530

    new tests

--- src/actions/TestCases/TestNgTestCase.java
+++ src/actions/TestCases/TestNgTestCase.java
@@ -3,18 +3,42 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import java.util.*;
 
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 public class TestNgTestCase {
 	
 	
 	@BeforeMethod
 	public void setUp()
 	{
-		System.out.println("inside setup");
+	   System.out.println("inside setup");
+       RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
 	}
 	@Test
 	public void test1()
 	{
-		System.out.println("inside test method");
+		given().when().get("/Hyderabad")
+		  .then().assertThat().statusCode(200);
+    }
+    @Test
+	public void test2()
+	{
+		given().when().get("/Hyderabad")
+		  .then().assertThat().statusLine("HTTP/1.1 200 OK");
+	}
+    @Test
+	public void test3()
+	{
+		given().when().get("/Hyderabad")
+		  .then().assertThat().header("Content-Type", equalTo("application/json"));
+	}
+    @Test
+	public void test4()
+	{
+		given().when().get("/Hyderabad")
+		  .then().assertThat().body("City", equalTo("Hyderabad"));
 	}
 	@AfterMethod
 	public void tearDown()
